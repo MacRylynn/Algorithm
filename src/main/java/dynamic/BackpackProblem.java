a2a78f3640abf5d9db396521c0d58bbc09bf5666
Macrylynn <530454062@qq.com>
2019-08-16 15:03:22 +0800

    create

---
+++ src/main/java/dynamic/BackpackProblem.java
@@ -0,0 +1,52 @@
+package dynamic;
+
+/**
+ * @ClassName BackpackProblem
+ * @Description
+ * @Author: lijiao73
+ * @Date: 2019/8/16 14:55
+ * @Version 1.0
+ */
+public class BackpackProblem {
+    public static void main(String[] args) {
+        int total = 5;
+        int[] w = {0, 2, 3, 4, 5};            //商品的体积2、3、4、5
+        int[] v = {0, 3, 4, 5, 6};            //商品的价值3、4、5、6
+        int bagV = 8;                            //背包大小
+        System.out.println(dpTest(total, w, v, bagV));
+    }
+    /**
+     * 理解这个问题的前提
+     * @param total      参数1：商品的总个数
+     * @param wight      参数2：每一个商品的体积（或重量等）
+     * @param value      参数3：每个商品对应的价值
+     * @param bagContain 参数4：背包总共能装下的商品体积（或重量）
+     * @return           返回结果：背包可以装下的最大价值和
+     */
+    private static int dpTest(int total, int[] wight, int[] value, int bagContain) {
+        if (wight.length != value.length || wight.length != total) {
+            return 0;
+        }
+        int[][] dp = new int[total][bagContain + 1];
+
+        for (int i = 1; i < total; i++) {
+            for (int j = 1; j <= bagContain; j++) {
+                if (j < wight[i]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - wight[i]] + value[i]);
+                }
+            }
+        }
+        for (int i = 0; i < total; i++) {
+            for (int j = 0; j <= bagContain; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        return dp[total - 1][bagContain];
+
+    }
+
+}
---
+++ src/main/java/sort/Sort.java
@@ -0,0 +1,161 @@
+package sort;
+
+/**
+ * @ClassName Sort
+ * @Description
+ * @Author: lijiao73
+ * @Date: 2019/8/16 14:44
+ * @Version 1.0
+ */
+public class Sort {
+    public static void main(String[] args) {
+
+    }
+
+    //交换一个数组中的两个元素
+    private static void swap(int[] org, int i, int j) {
+        int temp = org[i];
+        org[i] = org[j];
+        org[j] = temp;
+    }
+
+    //冒泡排序
+    private static void maopao(int[] org) {
+        if (org.length <= 1) {
+            return;
+        }
+        for (int i = 0; i < org.length - 1; i++) {
+            for (int j = 0; j < org.length - 1 - i; j++) {
+                if (org[j] > org[j + 1]) {
+                    swap(org, j, j + 1);
+                }
+            }
+        }
+    }
+
+    //直接插入排序
+    private static void charu(int[] org) {
+        if (org.length <= 1) {
+            return;
+        }
+        int index = 0;
+        int current = 0;
+        for (int i = 1; i < org.length; i++) {
+            index = i - 1;
+            current = org[i];
+            while (index >= 0 && org[index] > current) {
+                org[index + 1] = org[index];
+                index--;
+            }
+            org[index + 1] = current;
+
+        }
+    }
+
+    //简单选择牌组
+    private static void xuanze(int[] org) {
+        if (org.length <= 1) {
+            return;
+        }
+        int minIndex = 0;
+        for (int i = 0; i < org.length; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < org.length; j++) {
+                if (org[j] < org[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(org, minIndex, i);
+
+        }
+    }
+
+    //快速排序
+    private static void quickSort(int[] org, int i, int j) {
+        if (i >= j) {
+            return;
+        }
+        int mid = partion(org, i, j);
+        quickSort(org, i, mid - 1);
+        quickSort(org, mid + 1, j);
+    }
+
+    private static int partion(int[] org, int l, int r) {
+        int j = l;
+        int value = org[l];
+        for (int i = l + 1; i <= r; i++) {
+            if (org[i] < value) {
+                swap(org, i, j + 1);
+                j++;
+            }
+        }
+        swap(org, j, l);
+        return j;
+    }
+
+    //希尔排序
+    private static void xier(int[] org) {
+        if (org.length <= 1) {
+            return;
+        }
+        int index = 0;
+        int current = 0;
+        int length = org.length;
+        int h = 0;
+        while (h <= length) {
+            h = 3 * h + 1;
+        }
+        while (h >= 1) {
+            for (int i = h; i < org.length; i++) {
+                index = i - h;
+                current = org[i];
+                while (index >= 0 && org[index] > current) {
+                    org[index + h] = org[index];
+                    index = index - h;
+                }
+                org[index + h] = current;
+
+            }
+            h = (h - 1) / 3;
+        }
+    }
+
+    //堆排序
+    private static void HeapSort(int[] org) {
+        int len = org.length;
+        if (len < 1) {
+            return;
+        }
+        BuildHeap(org);//调整为大顶堆
+        while (len > 0) {
+            swap(org, 0, len - 1);//第一个元素和最后一个元素交换
+            len--;//类似于剔除最后一个元素
+            AdjustHeap(org, 0, len);
+        }
+    }
+
+    private static void BuildHeap(int[] org) {
+        int len = org.length;
+        for (int i = len / 2; i >= 0; i--) {
+            AdjustHeap(org, i, len);
+        }
+    }
+
+    private static void AdjustHeap(int[] org, int i, int len) {
+        int left = 2 * i;
+        int right = 2 * i + 1;
+        int maxIndex = i;
+        if (left < len && org[left] > org[maxIndex]) {
+            maxIndex = left;
+        }
+        if (right < len && org[right] > org[maxIndex]) {
+            maxIndex = right;
+        }
+        if (maxIndex != i) {
+            swap(org, i, maxIndex);
+            AdjustHeap(org, maxIndex, len);
+        }
+    }
+
+
+}
